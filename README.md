4635e81096b14226e2ac70501e96a8269a0177b2
Etienne Garnier <garnier.etienne@gmail.com>
2014-01-30 17:17:03 +0100

    init a new rails app

---
+++ README.md
@@ -0,0 +1,12 @@
+Paperless
+=========
+
+Paperless is a simple Personal Document Management app.
+
+Getting Started
+---------------
+
+```
+$/paperless/> bundle install --path vendor/bundle
+$/paperless/> bundle exec foreman start
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paperless::Application.config.secret_key_base = '9f8f3485a5238e6a46db66e5676438dfefc3eb024ed69f6fa2249c110ed1320b5fa50285d4bfb265703a520d5859b6962450a5413cd56510e1ac0e2ce4ead1ae'

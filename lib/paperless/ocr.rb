35380adb85d90aa8a955e6cb98cb04cc480ca727
Etienne Garnier <garnier.etienne@gmail.com>
2014-02-25 16:25:01 +0100

    pre process image before OCR to get better output

--- lib/paperless/ocr.rb
+++ lib/paperless/ocr.rb
@@ -7,10 +7,15 @@ module Paperless
     def self.extract(image_path)
       data = ""
       Dir.mktmpdir do |tmp|
-        `tesseract "#{image_path}" #{tmp}/result -l #{TESSERACT_LANGUAGE}`
+        `tesseract "#{pre_process_image(image_path, "#{tmp}/processed")}" #{tmp}/result -l #{TESSERACT_LANGUAGE}`
         data = IO.read "#{tmp}/result.txt"
       end
       data
+    end
+
+    def self.pre_process_image(image_path, new_image_path)
+      `convert "#{image_path}" -normalize -density 300 -depth 8 -threshold 50% #{new_image_path}.png`
+      "#{new_image_path}.png"
     end
   end
 end
